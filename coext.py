1bcb34e6137bb9e5206e5e8dbf3056a43cdd2f8e
jsstoni <jsstonha@gmail.com>
2019-09-25 06:13:21 -0300

    soporte para leer por numero de linea

--- coext.py
+++ coext.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, re
 import sys
 import html.parser
 
@@ -10,14 +10,29 @@ else:
 class coextCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.code = self.getCode()
-		m = re.search('(?=github:)\w.*', self.code)
-		if m:
-			ids = m.group(0)
+		reg_path = '(?:\[+[0-9, ]+\]github:)'
+		path = re.search('(?:\[+[0-9, ]+\])?(?=github:)\w.*', self.code)
+		if path:
+			ids = path.group(0)
 			grupo = 'github:'
-			urls = ids.replace(grupo, '')
-			with urllib.request.urlopen(urls) as url:
+			frag = re.search(reg_path, self.code)
+
+			if frag:
+				urls = re.sub(reg_path, '', ids)
+			else:
+				urls = ids.replace(grupo, '')
+
+			with urlopen(urls) as url:
 				data = url.read()
 				data = data.decode("utf8")
+			rl = data.split('\n')
+			lines = re.search('(?:\[+[0-9, ]+\])', ids)
+			if lines:
+				ln = lines.group(0)
+				ln = re.sub('(\[|\])', '', ln)
+				ln = ln.split(',')
+				data = rl[int(ln[0]) : int(ln[1])]
+				data = '\n'.join(data)
 			query = "%s" %(ids)
 			queryRegions = self.view.find_all ( query, sublime.LITERAL ) # literal flag is used to disable regex
 			for region in reversed ( queryRegions ): # use reverse region order to avoid displacing regions with previous replacements
